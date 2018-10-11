73171e3616c8be64ca51736fe77a7b48c6ea8994
Ben Formosa <ben.formosa@gmail.com>
2018-10-11 15:08:27 +1100

    convert simple report to return structured data

--- sat6-currency.py
+++ sat6-currency.py
@@ -1,138 +1,60 @@
-#!/usr/bin/python
+#!/usr/bin/env python2
+"""
+Satellite 6 version of 'spacewalk-report system-currency'
+"""
 
 import argparse
+import collections
+import csv
+import getpass
 import json
+import os
 import requests
 import sys
-import getpass
-import os
 import yaml
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 
-parser = argparse.ArgumentParser(
-    description="Satellite 6 version of 'spacewalk-report system-currency'"
-)
-parser.add_argument(
-    "-a", "--advanced",
-    action="store_true",
-    default=False,
-    help="Use this flag if you want to divide security errata by severity."
-    " Note: this will reduce performance if this script significantly."
-)
-parser.add_argument(
-    "-f", "--config",
-    type=argparse.FileType(mode='r'),
-    nargs='?',
-    help="Hammer CLI config file (defaults to ~/.hammer/cli_config.yml",
-    const=os.path.expanduser('~/.hammer/cli_config.yml')
-)
-parser.add_argument(
-    "-n", "--server",
-    type=str.lower,
-    help="Satellite server"
-)
-parser.add_argument(
-    "-u", "--username",
-    type=str,
-    help="Username to access Satellite"
-)
-parser.add_argument(
-    "-p", "--password",
-    type=str,
-    help="Password to access Satellite."
-    " The user will be asked interactively if password is not provided."
-)
-parser.add_argument(
-    "-s", "--search",
-    type=str,
-    required=False,
-    help="Search string for host."
-    "( like ?search=lifecycle_environment=Test",
-    default=('')
-)
-parser.add_argument(
-    "-l", "--library",
-    action="store_true",
-    required=False,
-    help="Use this flag to also report on Library Synced Content AND"
-    " to divide security errata by severity."
-    " Note: this will reduce performance of this script significantly."
-    " Use with -o, -e and -c options"
-)
-parser.add_argument(
-    "-o", "--organization",
-    type=str,
-    required=False,
-    help="Organization to use when using the '-l' option"
-)
-parser.add_argument(
-    "-c", "--contentview",
-    type=str,
-    required=False,
-    default="Default Organization View",
-    help="Content View to use using the '-l' option."
-    " Default: Default Organization View"
-)
-parser.add_argument(
-    "-e", "--environment",
-    type=str,
-    required=False,
-    default="Library",
-    help="Environment to use with the '-l' option. Default: Library"
-)
-args = parser.parse_args()
+def init(server):
+    """Set up global variables."""
+    global url, api, katello_api, post_headers, ssl_verify
 
-server = args.server
-username = args.username
-password = args.password
+    # Satellite specific parameters
+    if server.startswith('https://'):
+        url = server.strip('/')
+    else:
+        url = "https://{}".format(server)
+    api = url + "/api/"
+    katello_api = url + "/katello/api/v2"
+    post_headers = {'content-type': 'application/json'}
+    ssl_verify = True
 
-# If config option specified, read Hammer CLI config file
-if args.config is not None:
-    # Load the configuration file
-    config = yaml.safe_load(args.config)
+
+def loadconfig(configfile):
+    """
+    Return a tuple (server, username, password) of values from the
+    configuration file.
+
+    Values are None if not specified in the file.
+    """
+    config = yaml.safe_load(configfile)
     # If the key is present in the config file, attempt to load values
-    # Values specified on the commandline take precendence over config file
     key = ':foreman'
     try:
-        if args.server is None:
-            server = config[key][':host']
-        else:
-            server = args.server
+        server = config[key][':host']
     except(KeyError):
         pass
     try:
-        if args.username is None:
-            username = config[key][':username']
-        else:
-            username = args.username
+        username = config[key][':username']
     except(KeyError):
         pass
     try:
-        if args.password is None:
-            password = config[key][':password']
-        else:
-            password = args.password
+        password = config[key][':password']
     except(KeyError):
         pass
 
-if server is None:
-    raise ValueError("Server was not specified")
-if username is None:
-    raise ValueError("Username was not specified")
-if password is None:
-    password = getpass.getpass()
-
-# Satellite specific parameters
-if server.startswith('https://'):
-    url = server.strip('/')
-else:
-    url = "https://{}".format(server)
-api = url + "/api/"
-katello_api = url + "/katello/api/v2"
-post_headers = {'content-type': 'application/json'}
-ssl_verify = True
+    return (server, username, password)
 
 
 def get_with_json(location, json_data):
@@ -161,29 +83,22 @@ def get_with_json(location, json_data):
         sys.exit(1)
 
 
-def simple_currency():
+def output_csv(output):
+    if output:
+        fieldnames = output[0].keys()
+        writer = csv.DictWriter(sys.stdout, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(output)
 
-    # Print headline
-    print(
-        ','.join(str(x) for x in [
-            "system_id",
-            "org_name",
-            "name",
-            "security",
-            "bug",
-            "enhancement",
-            "score",
-            "content_view",
-            "content_view_publish_date",
-            "lifecycle_environment",
-            "subscription_os_release",
-            "os_release",
-            "arch",
-            "subscription_status",
-            "comment"
-        ]
-                 )
-    )
+
+def simple_currency():
+    """
+    Simple form of the system currency report.
+
+    Return a list of dicts.
+    """
+
+    output = []
 
     # Get all hosts (alter if you have more than 10000 hosts)
     hosts = get_with_json(
@@ -203,26 +118,45 @@ def simple_currency():
                 "content_facet_attributes" in host and
                 host["content_facet_attributes"]["errata_counts"]
         ):
+            host_data = collections.OrderedDict([
+                ("system_id", host["id"]),
+                ("org_name", host["organization_name"]),
+                ("name", host["name"]),
+                ("security", None),
+                ("bug", None),
+                ("enhancement", None),
+                ("score", None),
+                ("content_view", None),
+                ("content_view_publish_date", None),
+                ("lifecycle_environment", None),
+                ("subscription_os_release", None),
+                ("os_release", None),
+                ("arch", None),
+                ("subscription_status", None),
+                ("comment", host["comment"]),
+            ])
+
             # Get each number of different kinds of erratas
-            errata_count_sec = host[
+            host_data["security"] = host[
                 "content_facet_attributes"]["errata_counts"]["security"]
-            errata_count_bug = host[
+            host_data["bug"] = host[
                 "content_facet_attributes"]["errata_counts"]["bugfix"]
-            errata_count_enh = host[
+            host_data["enhancement"] = host[
                 "content_facet_attributes"]["errata_counts"]["enhancement"]
-            content_view_name = host[
+            host_data["content_view"] = host[
                 "content_facet_attributes"]["content_view"]["name"]
             content_view_id = host[
                 "content_facet_attributes"]["content_view"]["id"]
-            lifecycle_environment = host[
+            host_data["lifecycle_environment"] = host[
                 "content_facet_attributes"]["lifecycle_environment"]["name"]
             lifecycle_environment_id = host[
                 "content_facet_attributes"]["lifecycle_environment"]["id"]
-            subscription_os_release = host[
+            host_data["subscription_os_release"] = host[
                 "subscription_facet_attributes"]["release_version"]
-            arch = host["architecture_name"]
-            subscription_status = host["subscription_status_label"]
-            os_release = host["operatingsystem_name"]
+            host_data["arch"] = host["architecture_name"]
+            host_data["subscription_status"] = (
+                host["subscription_status_label"])
+            host_data["os_release"] = host["operatingsystem_name"]
 
             content_view = get_with_json(
                 "{}/content_views/{}/content_view_versions?"
@@ -234,42 +168,23 @@ def simple_currency():
                 json.dumps({"per_page": "10000"})
             )["results"]
 
-            cv_date = content_view[0]["created_at"]
+            host_data["content_view_publish_date"] = (
+                content_view[0]["created_at"])
             if (
-                    errata_count_sec is None or
-                    errata_count_bug is None or
-                    errata_count_enh is None
+                    host_data["security"] is None or
+                    host_data["bug"] is None or
+                    host_data["enhancement"] is None
             ):
-                score = 0
+                host_data["score"] = 0
             else:
                 # Calculate weighted score
-                score = (
-                    errata_count_sec * factor_sec +
-                    errata_count_bug * factor_bug +
-                    errata_count_enh * factor_enh
+                host_data["score"] = (
+                    host_data["security"] * factor_sec +
+                    host_data["bug"] * factor_bug +
+                    host_data["enhancement"] * factor_enh
                 )
-
-            # Print result
-            print(
-                ','.join(str(x) for x in [
-                    host["id"],
-                    host["organization_name"],
-                    host["name"],
-                    errata_count_sec,
-                    errata_count_bug,
-                    errata_count_enh,
-                    score,
-                    content_view_name,
-                    cv_date,
-                    lifecycle_environment,
-                    subscription_os_release,
-                    os_release,
-                    arch,
-                    subscription_status,
-                    host["comment"],
-                ]
-                         )
-            )
+            output.append(host_data)
+    return output
 
 
 def advanced_currency():
@@ -421,6 +336,7 @@ def advanced_currency():
 
 
 def library_currency():
+    """Advanced form of the system currency report, with more errata detail."""
 
     if args.organization is None:
         print("Organization must be specified for Library report")
@@ -724,10 +640,114 @@ def library_currency():
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Satellite 6 version of 'spacewalk-report system-currency'"
+    )
+    parser.add_argument(
+        "-a", "--advanced",
+        action="store_true",
+        default=False,
+        help="Use this flag if you want to divide security errata by severity."
+        " Note: this will reduce performance if this script significantly."
+    )
+    parser.add_argument(
+        "-f", "--config",
+        type=argparse.FileType(mode='r'),
+        nargs='?',
+        help="Hammer CLI config file (defaults to ~/.hammer/cli_config.yml",
+        const=os.path.expanduser('~/.hammer/cli_config.yml')
+    )
+    parser.add_argument(
+        "-n", "--server",
+        type=str.lower,
+        help="Satellite server"
+    )
+    parser.add_argument(
+        "-u", "--username",
+        type=str,
+        help="Username to access Satellite"
+    )
+    parser.add_argument(
+        "-p", "--password",
+        type=str,
+        help="Password to access Satellite."
+        " The user will be asked interactively if password is not provided."
+    )
+    parser.add_argument(
+        "-s", "--search",
+        type=str,
+        required=False,
+        help="Search string for host."
+        "( like ?search=lifecycle_environment=Test",
+        default=('')
+    )
+    parser.add_argument(
+        "-l", "--library",
+        action="store_true",
+        required=False,
+        help="Use this flag to also report on Library Synced Content AND"
+        " to divide security errata by severity."
+        " Note: this will reduce performance of this script significantly."
+        " Use with -o, -e and -c options"
+    )
+    parser.add_argument(
+        "-o", "--organization",
+        type=str,
+        required=False,
+        help="Organization to use when using the '-l' option"
+    )
+    parser.add_argument(
+        "-c", "--contentview",
+        type=str,
+        required=False,
+        default="Default Organization View",
+        help="Content View to use using the '-l' option."
+        " Default: Default Organization View"
+    )
+    parser.add_argument(
+        "-e", "--environment",
+        type=str,
+        required=False,
+        default="Library",
+        help="Environment to use with the '-l' option. Default: Library"
+    )
+    parser.add_argument(
+        "--output",
+        choices=["csv"],
+        type=str,
+        required=False,
+        default="csv",
+        help="Output format"
+    )
+    args = parser.parse_args()
+
+    # If config option specified, read Hammer CLI config file
+    if args.config is not None:
+        (server, username, password) = loadconfig(args.config)
+
+    # Values specified on the commandline take precendence over config file
+    if args.server is not None:
+        server = args.server
+    if args.username is not None:
+        username = args.username
+    if args.password is not None:
+        password = args.password
+
+    if server is None:
+        raise ValueError("Server was not specified")
+    if username is None:
+        raise ValueError("Username was not specified")
+    if password is None:
+        password = getpass.getpass()
+
+    init(server)
 
     if args.advanced:
-        advanced_currency()
+        output = advanced_currency()
     elif args.library:
-        library_currency()
+        output = library_currency()
     else:
-        simple_currency()
+        output = simple_currency()
+
+    if args.output == 'csv':
+        output_csv(output)
