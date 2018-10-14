1b673921b5b12306acba50a0095b4556a30775b6
Ben Formosa <ben.formosa@gmail.com>
2018-10-15 09:04:08 +1100

    update tests for rename

--- tests/test_sat6_currency.py
+++ tests/test_sat6_currency.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python2
 
 import unittest
-import sys
-import os
 
 
 class TestImportModule(unittest.TestCase):
     def test_import(self):
-        sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)))
         import sat6_currency
         self.assertTrue(True)
+
 
 if __name__ == "__main__":
     unittest.main()
