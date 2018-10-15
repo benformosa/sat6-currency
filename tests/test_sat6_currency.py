f5907d05a151c4435dd8d2052e52667470c10dba
Ben Formosa <ben.formosa@gmail.com>
2018-10-15 14:35:20 +1100

    remove import test

--- tests/test_sat6_currency.py
+++ tests/test_sat6_currency.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python2
 
+import sat6_currency
+
 import unittest
 
 
-class TestImportModule(unittest.TestCase):
-    def test_import(self):
-        import sat6_currency
-        self.assertTrue(True)
 
 
 if __name__ == "__main__":
