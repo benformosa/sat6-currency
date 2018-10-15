3ff7934529b45f33a6398d0c1d347497db2495bd
Ben Formosa <ben.formosa@gmail.com>
2018-10-15 15:10:45 +1100

    test output

--- tests/test_sat6_currency.py
+++ tests/test_sat6_currency.py
@@ -2,6 +2,7 @@
 
 import sat6_currency
 
+import collections
 import unittest
 
 
@@ -26,6 +27,73 @@ class TestLoadConfig(unittest.TestCase):
             self.assertEqual(server, None)
             self.assertEqual(username, None)
             self.assertEqual(password, None)
+
+
+class TestOutput(unittest.TestCase):
+    def setUp(self):
+        self.output = [
+            collections.OrderedDict([
+                ('name', 'one'),
+                ('number', 11),
+                ('colour', 'green'),
+            ]),
+            collections.OrderedDict([
+                ('name', 'two'),
+                ('number', 22),
+                ('colour', None),
+            ]),
+            collections.OrderedDict([
+                ('name', 'three'),
+                ('number', None),
+                ('colour', 'red'),
+            ]),
+        ]
+
+    def test_output_csv(self):
+        self.assertEqual(
+            sat6_currency.output_csv(self.output),
+            "name,number,colour\n"
+            "one,11,green\n"
+            "two,22,None\n"
+            "three,None,red"
+        )
+
+    def test_output_json(self):
+        self.assertEqual(
+            sat6_currency.output_json(self.output),
+            '[\n'
+            '  {\n'
+            '    "colour": "green",\n'
+            '    "name": "one",\n'
+            '    "number": 11\n'
+            '  },\n'
+            '  {\n'
+            '    "colour": null,\n'
+            '    "name": "two",\n'
+            '    "number": 22\n'
+            '  },\n'
+            '  {\n'
+            '    "colour": "red",\n'
+            '    "name": "three",\n'
+            '    "number": null\n'
+            '  }\n'
+            ']'
+        )
+
+    def test_output_yaml(self):
+        self.assertEqual(
+            sat6_currency.output_yaml(self.output),
+            "---\n"
+            "- colour: green\n"
+            "  name: one\n"
+            "  number: 11\n"
+            "- colour: null\n"
+            "  name: two\n"
+            "  number: 22\n"
+            "- colour: red\n"
+            "  name: three\n"
+            "  number: null\n"
+        )
 
 
 if __name__ == "__main__":
